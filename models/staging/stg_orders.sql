4883ff86f7e884e0049877633a84da4176de2d7e
ramesh reddy <rameshreddy.r2506@gmail.com>
2024-01-02 05:29:31 +0000

    developing stage orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+select 
+--from raw orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordercostprice,
+o.ordersellingprice,
+ordersellingprice - ordercostprice as orderprofit,
+--from raw customers
+c.customername,
+c.segment,
+c.country,
+--from raw product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} o
+left join {{ ref('raw_customers') }} c
+on o.customerid=c.customerid
+left join {{ ref('raw_product') }} p 
+on o.productid=p.productid
